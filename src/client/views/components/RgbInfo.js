f32ce1c6292046f4fe2aecd82dedb279135834c7
jgi <public-devgit-common@gissehel.org>
2019-09-15 16:56:46 +0200

    Let the color values be clickable and copy the value to clipboard

--- src/client/views/components/RgbInfo.js
+++ src/client/views/components/RgbInfo.js
@@ -5,6 +5,7 @@ import { Input, Ref, Segment } from 'semantic-ui-react';
 
 import './RgbInfo.css';
 import { ColorSquare } from './ColorSquare';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class SelectedInput extends React.Component {
     componentDidMount() {
@@ -40,16 +41,34 @@ const RgbInfo = ({ rgb, hcwb, hsl, hsv, color }) => {
         <SelectedInput value={color} />
         <Segment inverted>
             <ColorSquare width={'1em'} color={color} />
-            {color}
+            <CopyToClipboard text={color}>
+                <span className='toClipboard'>{color}</span>
+            </CopyToClipboard>
             &nbsp;
-            rgb(<ColorComponent value={rgb[0]} />, <ColorComponent value={rgb[1]} />, <ColorComponent value={rgb[2]} />)
+            -
             &nbsp;
-            hcwb(<ColorComponent value={hcwb[0]} />, <ColorComponent value={hcwb[1]} />%, <ColorComponent value={hcwb[2]} />%, <ColorComponent value={hcwb[3]} />%)
-            &nbsp;
-            hsl(<ColorComponent value={hsl[0]} />, <ColorComponent value={hsl[1]} />%, <ColorComponent value={hsl[2]} />%)
-            &nbsp;
-            hsv(<ColorComponent value={hsv[0]} />, <ColorComponent value={hsv[1]} />%, <ColorComponent value={hsv[2]} />%)
-        </Segment>
+            <CopyToClipboard text={`rgb(${rgb[0]},${rgb[1]},${rgb[2]})`}>
+                <span className='toClipboard'>rgb(<ColorComponent value={rgb[0]} />, <ColorComponent value={rgb[1]} />, <ColorComponent value={rgb[2]} />)</span>
+            </CopyToClipboard>
+			&nbsp;
+            -
+			&nbsp;
+            <CopyToClipboard text={`hcwb(${hcwb[0]},${hcwb[1]},${hcwb[2]},${hcwb[3]})`}>
+                <span className='toClipboard'>hcwb(<ColorComponent value={hcwb[0]} />, <ColorComponent value={hcwb[1]} />%, <ColorComponent value={hcwb[2]} />%, <ColorComponent value={hcwb[3]} />%)</span>
+            </CopyToClipboard>
+			&nbsp;
+            -
+			&nbsp;
+            <CopyToClipboard text={`hsl(${hsl[0]},${hsl[1]}%,${hsl[2]}%)`}>
+                <span className='toClipboard'>hsl(<ColorComponent value={hsl[0]} />, <ColorComponent value={hsl[1]} />%, <ColorComponent value={hsl[2]} />%)</span>
+            </CopyToClipboard>
+			&nbsp;
+            -
+			&nbsp;
+            <CopyToClipboard text={`hsv(${hsv[0]},${hsv[1]}%,${hsv[2]}%)`}>
+                <span className='toClipboard'>hsv(<ColorComponent value={hsv[0]} />, <ColorComponent value={hsv[1]} />%, <ColorComponent value={hsv[2]} />%)</span>
+            </CopyToClipboard>
+		</Segment>
     </div>;
 }
 
